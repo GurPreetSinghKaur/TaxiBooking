916eb399c022139fad9514fd6fc9139813785e3f
GurPreet SinghKaur <gurpreet122p@hotmail.es>
2024-06-27 00:19:16 +0100

    Removed duplicated Header from all the HTML pages

--- src/main/java/gurpreet/project/taxibooking/controller/MyController.java
+++ src/main/java/gurpreet/project/taxibooking/controller/MyController.java
@@ -5,6 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyController {
     @GetMapping(path = {"","home","welcome","index"})
-    public String welcomeView() {return "index";}
+    public String getHomePage() {return "index";}
+    @GetMapping("about")
+    public String getAboutPage() {return "about";}
+    @GetMapping("cars")
+    public String getCarPage() {return "cars";}
+    @GetMapping("services")
+    public String getServicesPage() {return "services";}
+    @GetMapping("contacts")
+    public String getContactsPage() {return "contacts";}
 
 }
