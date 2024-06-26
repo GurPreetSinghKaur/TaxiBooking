1c2241130924e3ec27a9073a2d39f9c3b691c298
GurPreet SinghKaur <gurpreet122p@hotmail.es>
2024-06-26 23:35:11 +0100

    updated files

---
+++ src/main/java/gurpreet/project/taxibooking/controller/MyController.java
@@ -0,0 +1,10 @@
+package gurpreet.project.taxibooking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class MyController {
+    @GetMapping(path = {"","home","welcome","index"})
+    public String welcomeView() {return "index";}
+
+}
