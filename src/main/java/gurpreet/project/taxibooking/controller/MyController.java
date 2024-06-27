d4a49818e7e0f7dd2e040d0caf0387b0bc76abf7
GurPreet SinghKaur <gurpreet122p@hotmail.es>
2024-06-27 11:45:02 +0100

    Added validation through annotations

--- src/main/java/gurpreet/project/taxibooking/controller/MyController.java
+++ src/main/java/gurpreet/project/taxibooking/controller/MyController.java
@@ -1,9 +1,13 @@
 package gurpreet.project.taxibooking.controller;
 
+import gurpreet.project.taxibooking.model.ContactForm;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -29,11 +33,15 @@ public class MyController {
     @GetMapping("contacts")
     public String getContactsPage(HttpServletRequest req, Model model) {
         String requestURI = req.getRequestURI();
-        model.addAttribute("currentPage",requestURI);return "contacts";}
+        model.addAttribute("currentPage",requestURI);
+        model.addAttribute("contactForm", new ContactForm());
+        return "contacts";}
 
    @PostMapping("contactForm")
-    public String contactForm(Model model){
-        return "";
+    public String contactForm(Model model, @Valid @ModelAttribute ContactForm contactForm, BindingResult bindingResult){
+        System.out.println(contactForm);
+   if (bindingResult.hasErrors())     System.out.println(bindingResult);
+        return "contacts";
 
     }
 
--- src/main/java/gurpreet/project/taxibooking/model/ContactForm.java
+++ src/main/java/gurpreet/project/taxibooking/model/ContactForm.java
@@ -1,10 +1,27 @@
 package gurpreet.project.taxibooking.model;
+import jakarta.validation.constraints.*;
+import lombok.*;
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class ContactForm {
 
-public class ContactForm
-{
+    private int id;
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 2, max = 30, message = "Invalid name size")
+    @Pattern(regexp = "[a-bA-B]")
     private String name;
-    private String phone;
+    @NotNull
+    @Min( value = 1000000000, message = "Phone number should be at least 10 digits")
+    @Min( value = 99999999999999L, message = "Phone number should not be  more than 15 digits")
+    private Long phone;
+    @NotEmpty(message = "Email cannot be empty")
+    @Size(min = 2, max = 30, message = "Invalid email size")
     private String email;
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 2, max = 500, message = "Invalid message size")
     private String message;
 
 }
