9016701abd8f3d0865121ea7d90062c3b1577bb2
GurPreet SinghKaur <gurpreet122p@hotmail.es>
2024-06-27 21:33:26 +0100

    Commented all the TMForm.js and added BookingForm model

--- src/main/java/gurpreet/project/taxibooking/model/BookingForm.java
+++ src/main/java/gurpreet/project/taxibooking/model/BookingForm.java
@@ -1,2 +1,29 @@
-package gurpreet.project.taxibooking.model;public class BookingForm {
+package gurpreet.project.taxibooking.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class BookingForm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String from;
+    private String to;
+    private String message;
+    private LocalDate date;
+    private LocalTime time;
+    private String comfort;
+    private int adult;
+    private int child;
 }
